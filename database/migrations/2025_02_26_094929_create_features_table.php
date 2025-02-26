4fdd289fc842fbf8c74c00ce7da99c530faf6681
GM Brhylle <gmjheizon027@gmail.com>
2025-02-26 18:47:31 +0800

    Definition and declaration of the comments, features and upvote migration table

---
+++ database/migrations/2025_02_26_094929_create_features_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('features', function (Blueprint $table) {
+            $table->id();
+
+            // post name
+            $table->string('name');
+
+            // post details
+            $table->string('description')->nullable();
+
+            // user who created the post
+            $table->foreignIdFor(\App\Models\User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('features');
+    }
+};
